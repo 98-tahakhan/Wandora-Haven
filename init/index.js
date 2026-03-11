429bfef7c83b60b2675e87aed4b71d0b7f489046
Taha Khan <khantaha636@gmail.com>
2026-03-12 02:15:18 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/Wandorahaven";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "69a173acc00666ae05059404",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("Data was initialized");
+};
+
+initDB();
